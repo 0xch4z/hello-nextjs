443be4949049e50c619a91cfa146a0667dee05bb
Charles Kenney <Charlesc.kenney@gmail.com>
2017-08-03 02:43:51 -0400

    added using-shared-components module

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-// This is Link API
-import Link from 'next/link'
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link';
 
-const Index = () => (
-  <div>
-    <Link href="/about">
-      <a>About Page</a>
-    </Link>
-    <p>Hello Next.js</p>
-  </div>
-)
+const PostLink = (props) => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+);
 
-export default Index
+export default () => (
+    <Layout>
+       <p>Hello Next.js</p>
+       <ul>
+           <PostLink title="Hello Next.js" />
+           <PostLink title="Learn Next.js is awesome" />
+           <PostLink title="Deploy apps with Zeit" />
+       </ul>
+    </Layout>
+);
